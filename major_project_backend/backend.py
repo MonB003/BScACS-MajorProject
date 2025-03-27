4a4ac5e5b5bd7fc063cf3c4bcb395f2496bf9ed6
Monica B <monicab3002@gmail.com>
2025-03-26 21:36:43 -0700

    Added timer to backend.py methods. Created testing method to write methods and times to a text file

--- major_project_backend/backend.py
+++ major_project_backend/backend.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify, send_file, make_response
 from flask_cors import CORS
-import database, hashing, files, security
-import os
-import datetime, jwt
+import database, hashing, files, security, testing
+import datetime, jwt, os, time
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -14,6 +13,10 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY") # Secure and unique key
 ALGORITHM = "HS256"  # HMAC SHA-256
 
+# Get env variable, default to "false", store as boolean
+test_mode_env = os.getenv("TEST_MODE", "false")
+TEST_MODE = True if test_mode_env.lower() == "true" else False
+
 # Set file upload and changes folders
 UPLOAD_FOLDER = 'uploaded-files/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -22,7 +25,13 @@ app.config['CHANGES_FOLDER'] = CHANGES_FOLDER
 
 @app.route("/upload-file", methods=['POST'])
 def handle_file_upload():
+    test_method = "handle_file_upload (error)"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+
     if 'file' not in request.files:
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'No file was sent.'}), 400
         
     # Get the file data from the request form
@@ -31,9 +40,11 @@ def handle_file_upload():
     user_id = request.form.get('user_id')
 
     if file.filename == '':
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'The filename cannot be empty.'}), 404
     
     if not user_id:
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'The user ID cannot be empty.'}), 400
     
     size = request.form.get('size')
@@ -46,6 +57,7 @@ def handle_file_upload():
 
     if not os.path.exists(full_file_path):
         error_message = "Error. The file path: " + full_file_path + " does not exist."
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': error_message}), 404
 
     if file:
@@ -79,11 +91,19 @@ def handle_file_upload():
         database.update_file_db(user_id, file.filename, file_hash, file.content_type, size, last_modified_date, file_path_form)
 
         message = "The file: " + file.filename + " was uploaded successfully."
+        test_method = f"handle_file_upload (file: {file.filename})"
+        testing.record_test_time(test_method, start_time)
         return jsonify({'message': message, 'file': file.filename}), 200
 
 @app.route("/check-file", methods=['POST'])
 def handle_file_check():
+    test_method = "handle_file_check (error)"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+        
     if 'file' not in request.files:
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'No file was sent.'}), 400
     
     # Get the file data from the request form
@@ -95,6 +115,7 @@ def handle_file_check():
     file_path_form = request.form.get('filePath')
 
     if file.filename == '':
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'The filename cannot be empty.'}), 404
     
     if file:
@@ -118,11 +139,14 @@ def handle_file_check():
         
         if not filename_result:
             error_message = "Error. The file: " + filename + " in the " + file_path_form + " directory was not found."
+            testing.record_test_time(test_method, start_time)
             return jsonify({'error': error_message}), 404
         
         differences_result = database.find_file_differences(filename_result, new_file_result)
         if differences_result is None:
             success_message = "Success! The file: " + filename + " has not changed."
+            test_method = f"handle_file_check (file not changed: {filename})"
+            testing.record_test_time(test_method, start_time)
             return jsonify({'message': success_message, 'file': filename_result['filename'], 'file_hash': filename_result['file_hash'], 'date': filename_result['date']}), 200
         else:
             # Decrypt the encrypted initial file
@@ -145,6 +169,8 @@ def handle_file_check():
             error_message = "Error. The file: " + filename + " has changed."
             log_message = database.insert_log_db(user_id, filename, differences_result)
             
+            test_method = f"handle_file_check (file changed: {filename})"
+            testing.record_test_time(test_method, start_time)
             return jsonify({'error': error_message, 'log_message': log_message, 'log_file': content_changes_log}), 400
 
 # Utility function to make MongoDB documents JSON serializable
@@ -154,41 +180,62 @@ def serialize_file(file_doc):
 
 @app.route("/get-user-files", methods=['POST'])
 def get_user_files():
+    test_method = "get_user_files"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+        
     user_id = request.form.get('user_id')
     
     if not user_id:
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'The user ID cannot be empty.'}), 400
     
     user_files_result = database.get_user_files(user_id)
     if user_files_result is None:
+        testing.record_test_time(test_method, start_time)
         return jsonify({'message': "The user has no files."}), 404
     else:
         # Convert cursor to a list of JSON-serializable dictionaries
         user_files = [serialize_file(file) for file in user_files_result]
+        testing.record_test_time(test_method, start_time)
         return jsonify({'message': "Success! The user has files.", 'files': user_files}), 200
 
 @app.route("/generate-log-file", methods=['POST'])
 def download_log_file():
+    test_method = "download_log_file"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+        
     user_id = request.form.get('user_id')
     username = request.form.get('username')
     log_file_path = database.generate_log_file(user_id, username)
 
+    testing.record_test_time(test_method, start_time)
     # Return the PDF log file to download
     return send_file(log_file_path, as_attachment=True, download_name=f"{username}-logs.pdf")
 
 @app.route("/signup", methods=['POST'])
 def handle_user_signup():
+    test_method = "handle_user_signup (error)"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+        
     # Get the username and password from the request form
     username = request.form.get('username')
     password = request.form.get('password')
     print("Signup for user", username)
 
     if username == '' or password == '':
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'Please fill out all fields.'}), 400
     
     if username:
         username_result = database.find_username(username)
         if username_result:
+            testing.record_test_time(test_method, start_time)
             return jsonify({'error': 'An account with this username already exists.'}), 400
         else:
             # Store new user
@@ -198,21 +245,30 @@ def handle_user_signup():
             response = make_response(jsonify({'message': 'Login successful'}))
             response.set_cookie('access_token', access_token, httponly=True, secure=True, samesite='Strict', max_age=datetime.timedelta(hours=1))
             
+            test_method = f"handle_user_signup (user: {user_result['username']})"
+            testing.record_test_time(test_method, start_time)
             return jsonify({'message': 'Success: A user account has been created.', 'user_id': user_result['userID'], 'username': user_result['username'], "access_token": access_token}), 200
 
 @app.route("/login", methods=['POST'])
 def handle_user_login():
+    test_method = "handle_user_login (error)"
+    start_time = None
+    if TEST_MODE:
+        start_time = time.perf_counter()  # Start timer
+        
     # Get the username and password from the request form
     username = request.form.get('username')
     password = request.form.get('password')
     print("Login for user", username)
 
     if username == '' or password == '':
+        testing.record_test_time(test_method, start_time)
         return jsonify({'error': 'Please fill out all fields.'}), 400
     
     if username and password:
         user_result = database.find_user_account(username, password)
         if not user_result:
+            testing.record_test_time(test_method, start_time)
             return jsonify({'error': 'No user account exists with this information.'}), 404
         else:
             # Setup access token
@@ -220,6 +276,8 @@ def handle_user_login():
             response = make_response(jsonify({'message': 'Login successful'}))
             response.set_cookie('access_token', access_token, httponly=True, secure=True, samesite='Strict', max_age=datetime.timedelta(hours=1))
 
+            test_method = f"handle_user_login (user: {user_result['username']})"
+            testing.record_test_time(test_method, start_time)
             return jsonify({'message': 'Success: A user account was found.', 'user_id': user_result['userID'], 'username': user_result['username'], "access_token": access_token}), 200
 
 def create_access_token(user_id):
---
+++ major_project_backend/testing.py
@@ -0,0 +1,25 @@
+import os, time
+
+# Get env variable, default to "false", store as boolean
+test_mode_env = os.getenv("TEST_MODE", "false")
+TEST_MODE = True if test_mode_env.lower() == "true" else False
+TIME_PRECISION = 4
+TEST_FILE = f"testing_times_{os.name}.txt"
+
+def record_test_time(method, start_time):
+    # print("RECORD TEST METHOD")
+    # print(f"TEST_MODE: {TEST_MODE}")
+    # print(os.getcwd())
+    
+    if TEST_MODE is False:
+        return
+    
+    # If program is being tested, record time taken
+    end_time = time.perf_counter()  # End timer
+    total_time = round(end_time - start_time, TIME_PRECISION)  # Round results
+
+    print(f"Writing to {TEST_FILE}: {method} took {total_time} seconds")
+
+    # Log time to a file
+    with open(TEST_FILE, "a") as file:
+        file.write(f"Method: {method}. Time: {total_time} seconds\n")
